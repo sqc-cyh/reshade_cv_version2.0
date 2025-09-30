262d4c48b225320b486ef24cebd81c2bd98f5888
songluan <456>
2025-09-30 20:36:15 +0800

    更新RDR2和RoR2游戏Hacking代码,RoR2还需后续对齐，修改GTAV和赛博朋克2077坐标系到OPENGL

--- mod_scripts/RDR2_camera.cs
+++ mod_scripts/RDR2_camera.cs
@@ -1,5 +1,5 @@
-using GTA;
-using GTA.Math;
+using RDR2;
+using RDR2.Math;
 using System;
 using System.Runtime.InteropServices;
 
@@ -7,7 +7,7 @@ public class CamInfoBufferSigned : Script
 {
     private static double[] buf = new double[17];
     private static GCHandle h;
-    private double counter = 1.0;   
+    private double counter = 1.0;
     private const double TRIGGER = 1.38097189588312856e-12;
     public CamInfoBufferSigned()
     {
@@ -16,12 +16,12 @@ public class CamInfoBufferSigned : Script
         this.Aborted += OnAbort;
 
         h = GCHandle.Alloc(buf, GCHandleType.Pinned);
-        buf[0] = TRIGGER; 
+        buf[0] = TRIGGER;
     }
 
     private void OnAbort(object sender, EventArgs e)
     {
-        try { if (h.IsAllocated) h.Free(); } catch {}
+        try { if (h.IsAllocated) h.Free(); } catch { }
     }
     private static void ColumnMajorCam2World(Matrix R, Vector3 C, double[] dst, int off)
     {
@@ -30,7 +30,6 @@ public class CamInfoBufferSigned : Script
         dst[off + 4] = R.M12; dst[off + 5] = R.M32; dst[off + 6] = -R.M22; dst[off + 7] = C.Y;
         dst[off + 8] = R.M13; dst[off + 9] = R.M33; dst[off +10] = -R.M23; dst[off +11] = C.Z;
     }
-
 
     private void OnTick(object sender, EventArgs e)
     {
@@ -46,19 +45,6 @@ public class CamInfoBufferSigned : Script
 
             // 注意顺序：Pitch -> Roll -> Yaw
             Matrix R = Matrix.RotationX(rx) * Matrix.RotationY(ry) * Matrix.RotationZ(rz);
-
-            // 左手系转右手系（如果需要）
-            R.M13 = -R.M13; R.M23 = -R.M23; R.M33 = -R.M33;
-            C.Z = -C.Z;
-
-            // OpenCV -> Open3D：Y 轴取反
-            R.M12 = -R.M12; R.M22 = -R.M22; R.M32 = -R.M32;
-            C.Y = -C.Y;
-            C *= 3.0f;
-            // // 存储列主序 c2w
-            // ColumnMajorCam2World(R, C, buf, 2);
-            // buf[14] = (double)fovDeg;
-
 
             counter = counter + 1.0;
             if (counter < 1.0) counter = 1.0;
@@ -76,7 +62,7 @@ public class CamInfoBufferSigned : Script
             {
                 double v = buf[2 + i];
                 allsum += v;
-                if ((i + 1) % 2 == 0) 
+                if ((i + 1) % 2 == 0)
                     plusminus += v;
                 else
                     plusminus -= v;
@@ -86,5 +72,4 @@ public class CamInfoBufferSigned : Script
         }
         catch { }
     }
-
 }
